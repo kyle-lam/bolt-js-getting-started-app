0f8115808aff99d12112ad3e8270838db7e3b8fc
Kyle Lam <kyle.lam.128@gmail.com>
2022-02-02 19:31:03 -0600

    hardcode listener

--- listeners/actions/index.js
+++ listeners/actions/index.js
@@ -2,9 +2,18 @@
 const { googleAuthInitiateCallback } = require('./auth-google');
 
 module.exports.register = (app) => {
-    app.action(
-      { action_id: 'auth-google-initiate', type: 'button_click' },
-      googleAuthInitiateCallback,
-    );
-  };
+  app.action('auth-google-initiate', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the google auth button`);
+  });
+}
+
+
+// module.exports.register = (app) => {
+//     app.action(
+//       { action_id: 'auth-google-initiate', type: 'button_click' },
+//       googleAuthInitiateCallback,
+//     );
+//   };
   
